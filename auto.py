b6d14890be384c917529625f6b4a5b9c03716359
One_Coder <1819918485@qq.com>
2021-05-15 12:04:35 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -102,8 +102,6 @@ class DaKa(object):
 def main(username, password):
     print("\n[Time] %s" % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     print("🚌 打卡任务启动")
-    print('username is: %s' % username)
-    print('password is: %s' % password)
     spinner = Halo(text='Loading', spinner='dots')
     spinner.start('正在新建打卡实例...')
     dk = DaKa(username, password)
